951932025308257dd0dcc1a9efaa7fd83021f561
Ikey Doherty <ikey@evolve-os.com>
2014-11-01 14:00:26 +0000

    Add initial skeleton structure

---
+++ src/budgie-radial-progress.c
@@ -0,0 +1,16 @@
+/*
+ * This file is part of libbudgie
+ *
+ * Copyright (C) 2014 Ikey Doherty
+ *
+ * libbudgie is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ */
+
+#include "budgie-radial-progress.h"
+
+void reserved(void)
+{
+}
---
+++ src/budgie-radial-progress.h
@@ -0,0 +1,12 @@
+/*
+ * This file is part of libbudgie
+ *
+ * Copyright (C) 2014 Ikey Doherty
+ *
+ * libbudgie is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ */
+
+#pragma once
